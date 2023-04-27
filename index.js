70ebaa0f393a9c6c8a3a34afbab5a626da47f1c2
nikita3006 <nikitanegi3011@gmail.com>
2023-04-27 10:48:14 -0700

    Dice Challenge

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //making dice name
+var randomImageSource = "images/" +  randomDiceImage; //where to find image
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" , randomImageSource); //which property change
+
+
+
+var randomNumber2 = Math.floor(Math.random() *6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"; //making dice name
+var randomImageSource2 = "images/" + randomDiceImage2 ;
+var image1 = document.querySelectorAll("img")[1];
+image1.setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2 ){
+    document.querySelector("h1").innerHTML = "Player 1 Wins! 🏆";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 1 Wins! 🏆";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+
+}
