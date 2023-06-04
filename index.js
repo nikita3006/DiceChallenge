962d9c4836a6f487f138e13082f14d8322bd66ff
nikita3006 <nikitanegi3011@gmail.com>
2023-06-04 20:09:41 +0530

    fixed diceChallenge

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ image1.setAttribute("src" , randomImageSource); //which property change
 var randomNumber2 = Math.floor(Math.random() *6) + 1;
 var randomDiceImage2 = "dice" + randomNumber2 + ".png"; //making dice name
 var randomImageSource2 = "images/" + randomDiceImage2 ;
-var image1 = document.querySelectorAll("img")[1];
-image1.setAttribute("src", randomImageSource2);
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
 
 if(randomNumber1 > randomNumber2 ){
     document.querySelector("h1").innerHTML = "Player 1 Wins! 🏆";
@@ -22,3 +22,4 @@ else{
     document.querySelector("h1").innerHTML = "Draw!";
 
 }
+
